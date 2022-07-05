bad629f159fa3da967862acf310922d832d4bf94
romanasv <romanasvenckus@gmail.com>
2022-07-05 10:29:03 +0300

    albums list added to index.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let postsWrapper = document.querySelector('#posts-wrapper');
+let albumsWrapper = document.querySelector('#albums-wrapper');
 
 fetch('https://jsonplaceholder.typicode.com/posts?_limit=15')
   .then(res => res.json())
@@ -79,4 +80,31 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15')
           })
         })
     });
+  })
+
+fetch('https://jsonplaceholder.typicode.com/albums?_limit=15')
+  .then(res => res.json())
+  .then(albums => {
+
+    albums.map(album => {
+      let albumItem = document.createElement('div');
+      albumItem.classList.add('album-item');
+
+      fetch('https://jsonplaceholder.typicode.com/users/' + album.userId)
+        .then(res => res.json())
+        .then(user => {
+
+          fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos?_limit=1`)
+            .then(res => res.json())
+            .then(photos => {
+              albumItem.innerHTML = `<h3><a href="./album.html">${album.title}</a></h3>
+                                     <div>Album created by: ${user.name}</div>
+                                     <img src="${photos[0].thumbnailUrl}">`;
+            })
+        })
+
+        albumsWrapper.prepend(albumItem);
+    })
+
+
   })
