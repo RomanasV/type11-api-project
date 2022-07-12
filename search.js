065f54f8d0349f645996b6040b8085e85ba3dd62
romanasv <romanasvenckus@gmail.com>
2022-07-12 10:58:06 +0300

    js search finished

--- search.js
+++ search.js
@@ -21,40 +21,47 @@ fetch(`https://jsonplaceholder.typicode.com/users?username=${searchPhrase}`)
   .then(res => res.json())
   .then(users => {
     if (users.length > 0) {
-      usersListTitle.textContent = 'Users:'
 
+
+      usersListTitle.textContent = 'Users:'
       users.map(user => {
         let userItem = document.createElement('li');
         userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
 
         usersList.append(userItem);
       })
+
+
     } else {
       fetch(`https://jsonplaceholder.typicode.com/users?name=${searchPhrase}`)
         .then(res => res.json())
         .then(usersByName => {
           if (usersByName.length > 0) {
+
+
             usersListTitle.textContent = 'Users:'
-      
             usersByName.map(user => {
               let userItem = document.createElement('li');
               userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-      
               usersList.append(userItem);
             })
+
+
           } else {
             fetch(`https://jsonplaceholder.typicode.com/users?email=${searchPhrase}`)
               .then(res => res.json())
               .then(usersByEmail => {
                 if (usersByEmail.length > 0) {
+
+
                   usersListTitle.textContent = 'Users:'
-            
                   usersByEmail.map(user => {
                     let userItem = document.createElement('li');
                     userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-            
                     usersList.append(userItem);
                   })
+
+
                 } else {
                   usersListTitle.textContent = 'Users not found.'
                 }
@@ -68,13 +75,16 @@ fetch(`https://jsonplaceholder.typicode.com/posts?title=${searchPhrase}`)
   .then(res => res.json())
   .then(posts => {
     if (posts.length > 0) {
+
+
       postsListTitle.textContent = 'Posts:';
       posts.map(post => {
         let postItem = document.createElement('li');
         postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title}</a>`;
-
         postsList.append(postItem);
       })
+
+
     } else {
       postsListTitle.textContent = 'Posts not found.';
     }
@@ -84,14 +94,15 @@ fetch(`https://jsonplaceholder.typicode.com/albums?title=${searchPhrase}`)
   .then(res => res.json())
   .then(albums => {
     if (albums.length > 0) {
-      albumsListTitle.textContent = 'Albums:';
 
+
+      albumsListTitle.textContent = 'Albums:';
       albums.map(album => {
         let albumItem = document.createElement('li');
         albumItem.innerHTML = `<a href="./album.html?album_id=${album.id}">${album.title}</a>`;
-
         albumsList.append(albumItem);
       })
+
 
     } else {
       albumsListTitle.textContent = 'Albums not found.';
@@ -104,6 +115,8 @@ searchPageForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   usersList.innerHTML = '';
+  postsList.innerHTML = '';
+  albumsList.innerHTML = '';
 
   let searchInput = event.target.elements['search-input'].value;
 
@@ -111,40 +124,46 @@ searchPageForm.addEventListener('submit', (event) => {
     .then(res => res.json())
     .then(users => {
       if (users.length > 0) {
-        usersListTitle.textContent = 'Users:'
 
+
+        usersListTitle.textContent = 'Users:'
         users.map(user => {
           let userItem = document.createElement('li');
           userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-
           usersList.append(userItem);
+
+
         })
       } else {
         fetch(`https://jsonplaceholder.typicode.com/users?name_like=${searchInput}`)
           .then(res => res.json())
           .then(usersByName => {
             if (usersByName.length > 0) {
+
+
               usersListTitle.textContent = 'Users:'
-        
               usersByName.map(user => {
                 let userItem = document.createElement('li');
                 userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-        
                 usersList.append(userItem);
               })
+
+
             } else {
               fetch(`https://jsonplaceholder.typicode.com/users?email_like=${searchInput}`)
                 .then(res => res.json())
                 .then(usersByEmail => {
                   if (usersByEmail.length > 0) {
+
+
                     usersListTitle.textContent = 'Users:'
-              
                     usersByEmail.map(user => {
                       let userItem = document.createElement('li');
                       userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-              
                       usersList.append(userItem);
                     })
+
+
                   } else {
                     usersListTitle.textContent = 'Users not found.'
                   }
@@ -152,5 +171,43 @@ searchPageForm.addEventListener('submit', (event) => {
             } 
           })
       }
-  })
+    })
+
+  fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${searchInput}`)
+    .then(res => res.json())
+    .then(posts => {
+      if (posts.length > 0) {
+
+
+        postsListTitle.textContent = 'Posts:';
+        posts.map(post => {
+          let postItem = document.createElement('li');
+          postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title}</a>`;
+          postsList.append(postItem);
+        })
+
+
+      } else {
+        postsListTitle.textContent = 'Posts not found.';
+      }
+    })
+
+  fetch(`https://jsonplaceholder.typicode.com/albums?title_like=${searchInput}`)
+    .then(res => res.json())
+    .then(albums => {
+      if (albums.length > 0) {
+  
+  
+        albumsListTitle.textContent = 'Albums:';
+        albums.map(album => {
+          let albumItem = document.createElement('li');
+          albumItem.innerHTML = `<a href="./album.html?album_id=${album.id}">${album.title}</a>`;
+          albumsList.append(albumItem);
+        })
+  
+  
+      } else {
+        albumsListTitle.textContent = 'Albums not found.';
+      }
+    })
 })
