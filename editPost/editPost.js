e5eb23881918a02c92026e371f50f72538174533
romanasv <romanasvenckus@gmail.com>
2022-07-22 09:05:30 +0300

    edit post

--- editPost/editPost.js
+++ editPost/editPost.js
@@ -1,3 +1,6 @@
+import { getAllUsers } from '../createPost/createPostController.js';
+import { renderOptionElement } from '../functions.js';
+
 async function init() {
   let queryParams = document.location.search;
   let urlParams = new URLSearchParams(queryParams);
@@ -6,9 +9,25 @@ async function init() {
   let res = await fetch('https://jsonplaceholder.typicode.com/posts/' + postId);
   let post = await res.json();
 
-  console.log(post.title);
-  console.log(post.body);
-  console.log(post.userId);
+  let users = await getAllUsers();
+  let selectElement = document.querySelector('#post-author');
+
+  users.map(item => {
+    renderOptionElement({
+      content: item.name,
+      value: item.id,
+      parentElement: selectElement,
+    });
+  })
+
+  const editPostForm = document.querySelector('#edit-post-form');
+  let postTitle = editPostForm.elements.title;
+  let postContent = editPostForm.elements.content;
+  let postAuthor = editPostForm.elements.author;
+
+  postTitle.value = post.title;
+  postContent.value = post.body;
+  postAuthor.value = post.userId;
 }
 
 init();
