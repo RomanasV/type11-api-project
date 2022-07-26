8d42d5160d1b79dea6f7c888265615e0f10231e2
romanasv <romanasvenckus@gmail.com>
2022-07-26 12:06:06 +0300

    pagination task

--- posts.js
+++ posts.js
@@ -40,9 +40,16 @@ function renderPostsByUserId(id) {
 }
 
 function renderAllPosts() {
-  fetch('https://jsonplaceholder.typicode.com/posts?_expand=user')
+  let urlParams = document.location.search;
+  let searchParams = new URLSearchParams(urlParams);
+  let limit = searchParams.get('limit') ? searchParams.get('limit') : 25;
+  let page = searchParams.get('page') ? searchParams.get('page') : 1;
+
+  fetch(`https://jsonplaceholder.typicode.com/posts?_expand=user&_page=${page}&_limit=${limit}`)
     .then(res => res.json())
     .then(posts => {
+      renderPaginationLinks(limit);
+
       postsListTitle.textContent = 'All Posts:';
       posts.map(post => {
         renderListElement({
@@ -53,6 +60,24 @@ function renderAllPosts() {
         });
       })
     })
+}
+
+function renderPaginationLinks(pageLimit) {
+  let total = 100;
+  let limit = pageLimit;
+  let pages = Math.ceil(total / limit);
+
+  let paginationWrapper = document.createElement('div');
+  paginationWrapper.classList.add('pagination-wrapper');
+
+  for (let i = 1; i <= pages; i++) {
+    let paginationLink = document.createElement('a');
+    paginationLink.href = `./posts.html?page=${i}&limit=${limit}`;
+    paginationLink.textContent = i;
+    paginationWrapper.append(paginationLink);
+  }
+
+  postsWrapper.append(paginationWrapper);
 }
 
 init();
